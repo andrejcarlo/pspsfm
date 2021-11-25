de7a5f853f289736ed61b30eb3863015e91b655c
Andrei-Carlo Papuc <andrei-carlo@hubs.com>
2021-11-25 01:02:55 +0100

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        async={true}
+      />
+
+      <Script strategy='lazyOnload' id="g-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+
+
+      <Component {...pageProps} />
+
+    </>
+  );
 }
 
 export default MyApp
